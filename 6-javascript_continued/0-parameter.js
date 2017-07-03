f2d647197fca58191b379c668335c2296f615287
syloc10 <sykhailoc@gmail.com>
2017-07-03 11:02:08 -0700

    0

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,5 +1,3 @@
-// Store a value passed by the user to the script in the variable my_parameter. Note it takes the second element which corresponds to the first parameter. Third element would correspond to second parameter
 let my_parameter = process.argv[2];
 
-// Print the my_parameter variable
 console.log(my_parameter)
